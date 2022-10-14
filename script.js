46dc1a9094dd0a7af72d001b522f26e7cebc134d
dejanV91 <dejanvukoicic91@gmail.com>
2022-10-14 19:19:18 +0200

    initial commit

--- script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("keydown", (e) => {
-    let code = e.keyCode;
-
-     let audio = document.querySelector(`audio[data-key= "${code}"]`);
-     audio.currentTime = 0;
-     audio.play();
-});
